f77fe6257e253b64137bc3ee8e944b22294afdda
ferriyusra <ferisakuragi@gmail.com>
2023-05-16 11:52:21 +0700

    refactor

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 
 async function createDatabase(connectionString) {
-	const pool = new Pool({
-		connectionString,
-	});
+  const pool = new Pool({
+    connectionString,
+  });
 
-	return pool.connect();
+  return pool.connect();
 }
 
 module.exports = {
-	createDatabase,
+  createDatabase,
 };
